99e8e8ac5a4effd42708a5ca0645b3d846f700a8
Sudharshan R <sudharshanrms2004@gmail.com>
2025-07-07 11:19:38 +0530

    Added article_citations migration

---
+++ database/migrations/2025_07_07_053537_create_article_citations_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('article_citations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("article_id")->constrained("articles", "id", "fk_article_citations_article_id");
+            $table->string("citation_year");
+            $table->string("citation_count");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('article_citations');
+    }
+};
